4cfb1aa78fd7f0270eb1be1e5595c735e25096de
Steve <stevelim3841@gmail.com>
2023-04-06 09:52:18 +0100

    Servo Tested

--- Pi/Rover_Test/main.py
+++ Pi/Rover_Test/main.py
@@ -13,6 +13,10 @@ INT2 = Button(19, pull_up = False)
 acc = (0.0, 0.0, 0.0)
 gyro = (0.0, 0.0, 0.0)
 
+gyroElapsed = 0.0;
+
+
+
 # Interupt callback function
 def LSM6DSO_readAcc():
 	global acc
@@ -22,11 +26,31 @@ def LSM6DSO_readAcc():
 	acc = (x, y, z)
 	
 def LSM6DSO_readGyro():
-	global gyro
+	global gyro, gyroElapsed
+	gyroTimer = time.time()
 	x = LSM6DSO.readGyroX(LSM6DSOAddr, i2cbus)
 	y = LSM6DSO.readGyroY(LSM6DSOAddr, i2cbus)
 	z = LSM6DSO.readGyroZ(LSM6DSOAddr, i2cbus)
+	gyroElapsed = gyroTimer - time.time()
 	gyro = (x, y, z)
+	return (x, y, z)
+	
+def IMU_Gyro_Cal():
+	gyroCal = []
+	i = 0
+	while i < 100:
+		gyroCal.append(LSM6DSO_readGyro())
+		i += 1
+	x = 0.0;
+	y = 0.0;
+	z = 0.0;
+	for j in range(len(gyroCal)):
+		temp = gyroCal[j]
+		x += temp[0]
+		y += temp[1]
+		z += temp[2]
+	
+	return (x/100, y/100, z/100)
 	
 INT1.when_pressed = LSM6DSO_readAcc
 INT2.when_pressed = LSM6DSO_readGyro
@@ -43,13 +67,20 @@ print("IMU initialised")
 
 ##### IMU end
 
+print("Calibrate Gyro")
+time.sleep(5)
+gyroCal = IMU_Gyro_Cal();
+print("Gyro calibrated: %.04f, %.04f, %.04f" % gyroCal)
+
+
 while True:
 	
 	
-	print("Acc: %.4f, %.4f, %.4f" % (acc))
+	#print("Acc: %.4f, %.4f, %.4f" % (acc))
 	
-	
-	print("Gyro: %.4f, %.4f, %.4f" % (gyro))
+	#print("Gyro: %.4f, %.4f, %.4f" % (gyro))
+	print("Yaw: %.4f" % (gyro[2] - gyroCal[2]))
+	print("Elapsed: %.4f" % (gyroElapsed))
 	print("")
 	time.sleep(0.01)
 	#continue
